f5fc55a03677e3d053d2b8b60aefede9bbe554d0
aliigamall <a.gamal@sieraj.com>
2018-04-03 13:38:16 +0300

    starter project

---
+++ src/scripts/scripts.js
@@ -0,0 +1,7 @@
+document.querySelector('.main-header__responsive-icon').addEventListener('click', function(){
+    document.querySelector('.main-header').classList.toggle('main-header--open');
+});
+
+document.querySelector('.main-header__overlay').addEventListener('click', function(){
+    document.querySelector('.main-header').classList.toggle('main-header--open');
+});
