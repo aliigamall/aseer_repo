ceac8592b2c2cebc99c8e8401de6618ce4c9df08
aliigamall <a.gamal@sieraj.com>
2018-04-09 16:45:42 +0300

    media center tabs and discover aseer

--- dist/scripts.js
+++ dist/scripts.js
@@ -11,9 +11,29 @@
 // fab button
 var fabComponent = document.querySelector('.fab');
 var fabButton = document.querySelector('.fab__trigger');
+
 if (fabComponent) {
     fabButton.addEventListener('click', function () {
         fabComponent.classList.toggle('is-opened');
+    });
+};
+
+// tabs
+var tabsComponent = document.querySelector('.tab-bar');
+var tabItems = document.querySelectorAll('.tab-bar__link');
+
+if (tabsComponent) {
+    [].forEach.call(tabItems, function (item) {
+        item.addEventListener('click', function (event) {
+            event.preventDefault();
+
+            var tabTarget = item.getAttribute("href");
+            document.querySelector('.tab-bar__item--active').classList.remove('tab-bar__item--active');
+            item.parentNode.classList.add('tab-bar__item--active');
+
+            document.querySelector('.tab-item--active').classList.remove('tab-item--active');
+            document.querySelector(tabTarget).classList.add('tab-item--active');
+        });
     });
 };
 //# sourceMappingURL=scripts.js.map
--- src/scripts/scripts.js
+++ src/scripts/scripts.js
@@ -9,8 +9,28 @@
 // fab button
 const fabComponent = document.querySelector('.fab');
 const fabButton = document.querySelector('.fab__trigger');
+
 if(fabComponent){
     fabButton.addEventListener('click', ()=>{
         fabComponent.classList.toggle('is-opened');
+    });
+};
+
+// tabs
+const tabsComponent = document.querySelector('.tab-bar');
+const tabItems = document.querySelectorAll('.tab-bar__link');
+
+if(tabsComponent){
+    [].forEach.call(tabItems, function(item) {
+        item.addEventListener('click', (event)=>{
+            event.preventDefault();
+
+            var tabTarget = item.getAttribute("href");
+            document.querySelector('.tab-bar__item--active').classList.remove('tab-bar__item--active');
+            item.parentNode.classList.add('tab-bar__item--active');
+
+            document.querySelector('.tab-item--active').classList.remove('tab-item--active');
+            document.querySelector(tabTarget).classList.add('tab-item--active');
+        });
     });
 };
